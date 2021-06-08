39bb670cadb471e5afc11fc7d776c786d492a7d7
pkss123 <80875171+pkss123@users.noreply.github.com>
2021-06-08 21:45:59 +0900

    Add files via upload
    파이썬 2일차

---
+++ Print_ex02.py
@@ -0,0 +1,5 @@
+a = "멍뭉이"
+b = "야옹이"
+print(a)
+print(b)
+print(a, b, sep="와")
---
+++ input_Quiz.py
@@ -0,0 +1,11 @@
+name = input("이름 : ")
+sex = input("성별 : ")
+age = int(input("나이 : "))
+birthYear = 2021 - age + 1
+
+print("-----------------")
+
+print("이름 :",name)
+print("성별 :",sex)
+print("나이 :",age)
+print("출생연도 : ",birthYear)
